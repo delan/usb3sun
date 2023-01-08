delan/usb3sun

commit 50582effc58439b34bc68170c964e646415e4b76

proper picoprobe support

src/main.cc
@@ -1,8 +1,14 @@
 #include "config.h"
 
+#ifdef PICOPROBE_ENABLE
+#define Sprint(...) (Serial1.print(__VA_ARGS__), Serial1.flush())
+#define Sprintln(...) (Serial1.println(__VA_ARGS__), Serial1.flush())
+#define Sprintf(...) (Serial1.printf(__VA_ARGS__), Serial1.flush())
+#else
 #define Sprint(...) (Serial.print(__VA_ARGS__), Serial.flush())
 #define Sprintln(...) (Serial.println(__VA_ARGS__), Serial.flush())
 #define Sprintf(...) (Serial.printf(__VA_ARGS__), Serial.flush())
+#endif
 
 #include <atomic>
 
@@ -35,9 +41,7 @@ tusb_desc_device_t desc_device;
 
 static Adafruit_SSD1306 display(128, 32, &Wire, /* OLED_RESET */ -1);
 
-#if defined(WAIT_PIN) || defined(WAIT_SERIAL)
 std::atomic<bool> wait = true;
-#endif
 
 struct {
   bool bell;
@@ -77,6 +81,41 @@ void setup() {
   display.drawXBitmap(0, 0, splash_bits, 128, 32, SSD1306_WHITE);
   display.display();
 
+#if defined(PICOPROBE_ENABLE)
+  Serial1.setPinout(PICOPROBE_TX, PICOPROBE_RX);
+  Serial1.setFIFOSize(4096);
+  Serial1.begin(115200, SERIAL_8N1);
+#elif defined(SUNK_ENABLE)
+  // gpio invert must be set *after* setPinout/begin
+  Serial1.setPinout(SUN_KTX, SUN_KRX);
+  Serial1.begin(1200, SERIAL_8N1);
+  gpio_set_outover(SUN_KTX, GPIO_OVERRIDE_INVERT);
+  gpio_set_inover(SUN_KRX, GPIO_OVERRIDE_INVERT);
+#endif
+#if defined(FAKE_SUN_ENABLE)
+  // gpio invert must be set *after* setPinout/begin
+  Serial2.setPinout(FAKE_SUN_KRX, FAKE_SUN_KTX);
+  Serial2.begin(1200, SERIAL_8N1);
+  gpio_set_outover(FAKE_SUN_KRX, GPIO_OVERRIDE_INVERT);
+  gpio_set_inover(FAKE_SUN_KTX, GPIO_OVERRIDE_INVERT);
+#elif defined(SUNM_ENABLE)
+  // gpio invert must be set *after* setPinout/begin
+  Serial2.setPinout(SUN_MTX, SUN_MRX);
+  Serial2.begin(115200, SERIAL_8N1);
+  gpio_set_outover(SUN_MTX, GPIO_OVERRIDE_INVERT);
+  gpio_set_inover(SUN_MRX, GPIO_OVERRIDE_INVERT);
+#endif
+
+  for (int i = 0; i < splash_height; i += 2) {
+    for (int j = 0; j < splash_width; j += 1) {
+      if (splash_bits[(splash_width + 7) / 8 * i + j / 8] >> j % 8 & 1)
+        Sprintf("\033[7m ");
+      else
+        Sprintf("\033[0m ");
+    }
+    Sprintf("\033[0m\n");
+  }
+
 #ifdef WAIT_PIN
   pinMode(WAIT_PIN, INPUT_PULLUP);
   while (digitalRead(WAIT_PIN));
@@ -84,30 +123,7 @@ void setup() {
 #ifdef WAIT_SERIAL
   while (Serial.read() == -1);
 #endif
-#if defined(WAIT_PIN) || defined(WAIT_SERIAL)
   wait = false;
-#endif
-
-  // gpio invert must be set *after* setPinout/begin
-  Serial1.setPinout(SUN_KTX, SUN_KRX);
-  Serial1.begin(1200, SERIAL_8N1);
-  gpio_set_outover(SUN_KTX, GPIO_OVERRIDE_INVERT);
-  gpio_set_inover(SUN_KRX, GPIO_OVERRIDE_INVERT);
-#ifdef FAKE_SUN_ENABLE
-  // gpio invert must be set *after* setPinout/begin
-  Serial2.setPinout(FAKE_SUN_KRX, FAKE_SUN_KTX);
-  Serial2.begin(1200, SERIAL_8N1);
-  gpio_set_outover(FAKE_SUN_KRX, GPIO_OVERRIDE_INVERT);
-  gpio_set_inover(FAKE_SUN_KTX, GPIO_OVERRIDE_INVERT);
-#else
-  // gpio invert must be set *after* setPinout/begin
-  Serial2.setPinout(SUN_MTX, SUN_MRX);
-  Serial2.begin(9600, SERIAL_8N1);
-  gpio_set_outover(SUN_MTX, GPIO_OVERRIDE_INVERT);
-  gpio_set_inover(SUN_MRX, GPIO_OVERRIDE_INVERT);
-#endif
-
-  Sprintln("usb3sun");
 
   digitalWrite(LED_BUILTIN, LOW);
 }
@@ -201,12 +217,11 @@ void loop() {
 #ifdef FAKE_SUN_ENABLE
   if (!fake.ok) {
     Serial2.write(SUNK_RESET);
-  } else {
-    ;
   }
 #endif
 }
 
+#ifdef SUNK_ENABLE
 void serialEvent1() {
   while (Serial1.available() > 0) {
     uint8_t command = Serial1.read();
@@ -251,6 +266,7 @@ void serialEvent1() {
     }
   }
 }
+#endif
 
 #ifdef FAKE_SUN_ENABLE
 void serialEvent2() {
@@ -282,9 +298,7 @@ void serialEvent2() {
 #endif
 
 void setup1() {
-#if defined(WAIT_PIN) || defined(WAIT_SERIAL)
   while (wait);
-#endif
 
   // Check for CPU frequency, must be multiple of 120Mhz for bit-banging USB
   uint32_t cpu_hz = clock_get_hz(clk_sys);
@@ -396,12 +410,14 @@ void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t cons
         }
       }
 
+#ifdef SUNK_ENABLE
       for (int i = 0; i < modifierChangesLen; i++) {
         // for DV bindings, make when key makes and break when key breaks
         for (int j = 0; j < sizeof(DV_BINDINGS) / sizeof(*DV_BINDINGS); j++)
           if (DV_BINDINGS[j].usbkModifier == modifierChanges[i].usbkModifier)
             Serial1.write(modifierChanges[i].make ? DV_BINDINGS[j].sunkMake : DV_BINDINGS[j].sunkBreak);
       }
+#endif
 
       // treat simultaneous DV and Sel changes as DV before Sel, for DV+Sel bindings
       state.lastModifiers = kreport->modifier;
@@ -464,6 +480,7 @@ void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t cons
           continue;
         }
 
+#ifdef SUNK_ENABLE
         bool consumedByDvSel = false;
 
         // for DV+Sel bindings:
@@ -486,6 +503,7 @@ void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t cons
           for (int j = 0; j < sizeof(SEL_BINDINGS) / sizeof(*SEL_BINDINGS); j++)
             if (SEL_BINDINGS[j].usbkSelector == selectorChanges[i].usbkSelector)
               Serial1.write(selectorChanges[i].make ? SEL_BINDINGS[j].sunkMake : SEL_BINDINGS[j].sunkBreak);
+#endif
       }
 
       // finally commit the Sel changes
@@ -507,13 +525,13 @@ void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t cons
           | (mreport->buttons & USBM_RIGHT ? SUNM_RIGHT : 0),
         (uint8_t) mreport->x, (uint8_t) -mreport->y, 0, 0,
       };
-#ifdef FAKE_SUN_ENABLE
-      Sprintf("\nmouse would send %02Xh %02Xh %02Xh %02Xh %02Xh (disabled by FAKE_SUN_ENABLE)",
-        result[0], result[1], result[2], result[3], result[4]);
-#else
+#ifdef SUNM_ENABLE
       Sprintf("\nmouse send %02Xh %02Xh %02Xh %02Xh %02Xh = %zu",
         result[0], result[1], result[2], result[3], result[4],
         Serial2.write(result, sizeof(result) / sizeof(*result)));
+#else
+      Sprintf("\nmouse send %02Xh %02Xh %02Xh %02Xh %02Xh (disabled)",
+        result[0], result[1], result[2], result[3], result[4]);
 #endif
 
       state.lastButtons = mreport->buttons;

platformio.ini
@@ -17,8 +17,8 @@ lib_deps =
     https://github.com/adafruit/Adafruit-GFX-Library.git
     https://github.com/adafruit/Adafruit_SSD1306.git
     https://github.com/sekigon-gonnoc/Pico-PIO-USB.git
-build_flags = -DUSE_TINYUSB -DCFG_TUSB_CONFIG_FILE=\"C:/Users/delan/code/pico/tusb_config.h\"
+build_flags = -DUSE_TINYUSB -DCFG_TUSB_CONFIG_FILE=\"C:/Users/delan/code/pico/tusb_config.h\" -DCFG_TUSB_DEBUG=3
 board_build.f_cpu = 120000000L
-; build_type = debug
-; debug_tool = picoprobe
-; upload_protocol = picoprobe
+build_type = debug
+debug_tool = picoprobe
+upload_protocol = picoprobe

src/config.h
@@ -21,9 +21,21 @@
                             //              pin 7: 0 V (yellow)
                             //              pin 8: +5 Vdc (orange)
 
-// fake Sun host for loopback testing (disables mouse support)
+// send output over Serial1 instead of Serial (disables Sun keyboard interface)
+#define PICOPROBE_ENABLE
+#if !defined(PICOPROBE_ENABLE)
+#define SUNK_ENABLE
+#endif
+#define PICOPROBE_BAUD 115200 // values other than 9600 may require PuTTY/minicom
+
+// fake Sun host for loopback testing (disables Sun mouse interface)
 // #define FAKE_SUN_ENABLE
+#if !defined(FAKE_SUN_ENABLE)
+#define SUNM_ENABLE
+#endif
 
 // the following must be GP# numbers valid for...
-#define FAKE_SUN_KRX 4      // ...UART1 TX: connect to SUN_KRX
-#define FAKE_SUN_KTX 5      // ...UART1 RX: connect to SUN_KTX
+#define FAKE_SUN_KRX 8      // ...UART1 TX: connect to SUN_KRX
+#define FAKE_SUN_KTX 9      // ...UART1 RX: connect to SUN_KTX
+#define PICOPROBE_TX 0      // ...UART0 TX: connect to picoprobe GP5
+#define PICOPROBE_RX 1      // ...UART0 RX: connect to picoprobe GP4

tusb_config.h
@@ -1,3 +1,7 @@
+// usb3sun note!
+// platformio incremental build is unaware of this file.
+// if you change it, do a clean build or touch the build_flags.
+
 /*
  * The MIT License (MIT)
  *
@@ -86,6 +90,12 @@
 #define CFG_TUD_HID_EP_BUFSIZE  (64)
 
 // ======= LOCAL ADDITIONS BELOW ======= //
+
+// see <.pio/libdeps/pico/Adafruit TinyUSB Library/src/arduino/ports/rp2040/Adafruit_TinyUSB_rp2040.cpp>
+#define CFG_TUSB_DEBUG_PRINTF serial1_printf
+// extern "C" {
+//   extern int serial2_printf(const char *__restrict __format, ...);
+// }
 
 #define CFG_TUH_ENABLED 1
 #define CFG_TUH_RPI_PIO_USB 1

commit 588b9d21de8e664b5734697f901e4f1f6b617f4e

fix bug where mouse baud was too high
(introduced in 50582effc58439b34bc68170c964e646415e4b76)

src/main.cc
@@ -104,7 +104,7 @@ void setup() {
 #elif defined(SUNM_ENABLE)
   // gpio invert must be set *after* setPinout/begin
   Serial2.setPinout(SUN_MTX, SUN_MRX);
-  Serial2.begin(115200, SERIAL_8N1);
+  Serial2.begin(SUN_MBAUD, SERIAL_8N1);
   gpio_set_outover(SUN_MTX, GPIO_OVERRIDE_INVERT);
   gpio_set_inover(SUN_MRX, GPIO_OVERRIDE_INVERT);
 #endif