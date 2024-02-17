1ba2980650c1c8e0cb2828b4dd4c69ca1de79807
Delan Azabani <delan@azabani.com>
2024-02-17 12:58:58 +0000

    NixOS: add picocom and fix debugging in vscode

--- shell.nix
+++ shell.nix
@@ -1,5 +1,7 @@
 # current status:
+# - picocom works
 # - vscode extension works
+# - debugging in vscode works
 # - build (pio run) works
 # - upload (pio run -t upload) works
 # - running openocd manually works
@@ -22,9 +24,13 @@ let
   envname = "platformio-fhs";
   mypython = pkgs.python3.withPackages (ps: with ps; [ platformio ]);
 in
-(pkgs.buildFHSUserEnv {
+(pkgs.buildFHSEnv {
   name = envname;
+
   targetPkgs = pkgs: (with pkgs; [
+    # picocom -fn -b115200 --imap lfcrlf /dev/ttyACM0
+    picocom
+
     # for pio cli and vscode extension
     platformio-core
     mypython
@@ -35,7 +41,15 @@ in
     # for running openocd via pio cli or vscode extension
     libusb1 # libusb-1.0.so.0
     hidapi # libhidapi-hidraw.so.0
+
+    # for platformio debugging in vscode
+    # (ldd ~/.platformio/packages/toolchain-rp2040-earlephilhower/bin/arm-none-eabi-gdb)
+    ncurses5 # libtinfo.so.5
+    mpfr # libmpfr.so.6
   ]);
-  # NixOS/nixpkgs#263201, NixOS/nixpkgs#262775, NixOS/nixpkgs#262080
-  runScript = "env LD_LIBRARY_PATH= bash";
+
+  # clear LD_LIBRARY_PATH (NixOS/nixpkgs#263201, NixOS/nixpkgs#262775, NixOS/nixpkgs#262080)
+  # then add path for platformio debugging in vscode
+  # (ldd ~/.platformio/packages/toolchain-rp2040-earlephilhower/bin/arm-none-eabi-gdb)
+  runScript = "env LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.ncurses5 ]} bash";
 }).env
