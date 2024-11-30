ea25238476a19b741196214123d255953d694e9d
Delan Azabani <delan@azabani.com>
2024-11-30 02:55:42 +0000

    [wip] initial rust ffi support (fails to build)

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+// Remove if STD is supported for your platform and you plan to use it
+#![no_std]
+
+// Remove if STD is supported for your platform and you plan to use it
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+//
+// Entry points
+//
+
+#[no_mangle]
+extern "C" fn arduino_setup() {
+}
+
+#[no_mangle]
+extern "C" fn arduino_loop() {
+}
