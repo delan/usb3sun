0896de8f77e6c2c2281ef81d2889614966eb8eac
Delan Azabani <delan@azabani.com>
2024-12-01 08:59:36 +0000

    rust: remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,3 @@
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
-
-//
-// Entry points
-//
-
-#[no_mangle]
-extern "C" fn arduino_setup() {
-}
-
-#[no_mangle]
-extern "C" fn arduino_loop() {
-}
